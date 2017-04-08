52f81d76f8f0159aed2e36145cf90545c9beae2b
pshriva <shrivastavapranjali@gmail.com>
2017-04-08 00:41:34 -0700

    Update MyUMLParser.java

--- MyUMLParser.java
+++ MyUMLParser.java
@@ -6,6 +6,8 @@ import japa.parser.ast.body.MethodDeclaration;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 public class MyUMLParser {
@@ -18,35 +20,50 @@ public class MyUMLParser {
 	static ArrayList<String> methodNames = new ArrayList<String>();
 	static ArrayList<String> fieldNames = new ArrayList<String>();
 	static ArrayList<String> subClassNames = new ArrayList<String>();
+	static ArrayList<String> associations = new ArrayList<String>();
 	static StringBuffer umlFile = new StringBuffer();
 	static CompilationUnit cu;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		inputfilepath = "F:/user/SJSU/202/PersonalProject/Samples";
-		parseFiles(inputfilepath);
-		for(String s:interfaceNames){
+		File file = new File(inputfilepath);
+		UMLGenerator umlgenerator = new UMLGenerator();
+		classNames = getJavaClasses(file);
+		parseFiles(file);
+		/*for(String s:interfaceNames){
 			System.out.println(s);
 		}
 		for(String s:methodNames){
 			System.out.println(s);
-		}
-		for(String s:fieldNames){
-			System.out.println(s);
-		}
-		for(String s:subClassNames){
+		}*/
+		
+		for(String s:associations){
 			System.out.println(s);
 		}
 		System.out.println(umlFile);
+		umlgenerator.createClassDiagram(umlFile.toString());
 	}
-	private static void parseFiles(String inputfilepath2) {
+	private static ArrayList<String> getJavaClasses(File file) {
+		ArrayList<String> classes = new ArrayList<String>();
 		// TODO Auto-generated method stub
-		File file = new File(inputfilepath);
 		File[] files = file.listFiles();
-		umlFile.append("@startuml\n");
 		for(File f: files){
 			if(f.getName().contains(".java")){
 				javaClassName = f.getName().split("\\.");
-				classNames.add(javaClassName[0]);
+				classes.add(javaClassName[0]);
+			}
+		}
+		
+		return classes;
+	}
+	private static void parseFiles(File file) {
+		File[] files = file.listFiles();
+		umlFile.append("@startuml\n");
+		
+		for(File f: files){
+			if(f.getName().contains(".java")){
+				javaClassName = f.getName().split("\\.");
+				//classNames.add(javaClassName[0]);
 				try {
 					cu = JavaParser.parse(f);
 
@@ -63,7 +80,6 @@ public class MyUMLParser {
 			}
 		}
 		getConnections();
-		umlFile.toString();
 	}
 	
 	public static void createFile(){
@@ -85,6 +101,9 @@ public class MyUMLParser {
 		}
 		for (String cName : subClassNames){
 			umlFile.append(cName + "\n");
+		}
+		for (String asName: associations){
+			umlFile.append(asName + "\n");
 		}
 		umlFile.append("@enduml");
 	}
@@ -134,6 +153,46 @@ public class MyUMLParser {
 		@Override
 		public void visit(FieldDeclaration fd, Void arg){
 			String field = null;
+			String arrayTypeObject = null;
+			// looks for if a class contains objects of some other class
+			for(String classname : classNames){
+				String associate = null;
+				String associate_reverse = null;
+				String reverse1 = null;
+				String reverse2 = null;
+				String allocate = null;
+				int flag = 0;
+				arrayTypeObject = "<" + classname + ">";
+				if(fd.getType().toString().equals(classname)){
+					associate = javaClassName[0] + " -- " + classname;
+					associate_reverse = classname + " -- " + javaClassName[0];
+					flag = 1;
+				}
+				else if(fd.getType().toString().contains(arrayTypeObject)){
+					associate = javaClassName[0] + " -- \"*\" " +classname;
+					associate_reverse = classname + " -- \"*\" " + javaClassName[0];
+					flag = 2;
+				}
+				if(associate != null && associations.contains(associate) == false && associations.contains(associate_reverse) == false){
+					associations.add(associate);
+				}else if(associate != null && associations.contains(associate) == false && associations.contains(associate_reverse)){
+					reverse1 = classname + " -- " + javaClassName[0];
+					reverse2 = classname + " -- \"*\" " + javaClassName[0];
+					if(associations.contains(reverse1) && flag == 2){
+						associations.remove(reverse1);
+						allocate = javaClassName[0] + " \"1\" -- \"*\" " +classname;
+						associations.add(allocate);
+					}else if(associations.contains(reverse2) && flag == 1){
+						associations.remove(reverse2);
+						allocate = javaClassName[0] + " \"*\" -- \"1\" " +classname;
+						associations.add(allocate);
+					}else if(associations.contains(reverse2) && flag == 2){
+						associations.remove(reverse2);
+						allocate = javaClassName[0] + " \"*\" -- \"*\" " +classname;
+						associations.add(allocate);
+					}
+				}
+			}
 			if(fd.getModifiers()==1){
 				field = "+ " + fd.getVariables().get(0) + " : " + fd.getType();
 				fieldNames.add(field);
@@ -143,5 +202,4 @@ public class MyUMLParser {
 			}
 		}
 	}
-
 }

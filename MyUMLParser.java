fd4fda1295fad5a241718ce4701f66222326f2f2
pshriva <shrivastavapranjali@gmail.com>
2017-04-13 11:56:02 -0700

    Update MyUMLParser.java

--- MyUMLParser.java
+++ MyUMLParser.java
@@ -1,36 +1,41 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-import net.sourceforge.plantuml.FileFormat;
-import net.sourceforge.plantuml.FileFormatOption;
-import net.sourceforge.plantuml.SourceStringReader;
+import japa.parser.ast.body.ConstructorDeclaration;
+import japa.parser.ast.visitor.VoidVisitorAdapter;
 
 
-
-public class UMLGenerator {
-
-	/*public void createUML(String fName) {
-		File src = new File(fName);
-		SourceFileReader read;
-		List<GeneratedImage> list;
-		try {
-			read = new SourceFileReader(src);
-			list = read.getGeneratedImages();
-			File pngImage = list.get(0).getPngFile();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}*/
-
-	public void createClassDiagram(String umlFile) throws IOException {
+public class ConstructorVisitor extends VoidVisitorAdapter<Void>{
+	static ArrayList<String> constructors = new ArrayList<String>();
+	static ArrayList<String> constructorDependency = new ArrayList<String>();
+	@Override
+	public void visit(ConstructorDeclaration n, Void arg) {
 		// TODO Auto-generated method stub
-		SourceStringReader reader = new SourceStringReader(umlFile);
-		FileOutputStream output = new FileOutputStream(new File("F:/user/SJSU/202/PersonalProject/Samples/testcase3/test.svg"));
-		 reader.generateImage(output, new FileFormatOption(FileFormat.SVG, false));
+		StringBuffer sb = new StringBuffer();
+		String method = null;
+		if(n.getParameters() != null && (n.getModifiers()==1 || n.getModifiers() == 1025)){
+			for(int i =0; i<n.getParameters().size(); i++){
+				sb.append(n.getParameters().get(i).getId() + " : " + n.getParameters().get(i).getType());
+				if(n.getParameters().size()>1 && i != n.getParameters().size()-1)
+					sb.append(" ");
+			}
+			method = "+ " + n.getName() + "(" +sb.toString() + ")";
+		}else if(n.getModifiers()==1 || n.getModifiers() == 1025){
+			method = "+ " + n.getName() + "()";
+		}
+		if(method != null){
+			constructors.add(method);
+		}
 	}
+	
+	public ArrayList<String> getConstructors(){
+		//System.out.println(constructors);
+		return constructors;
+	}
+
+	public ArrayList<String> getDependency() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }

dd670d43f28ac1f39fc6c91a9c69af4ce213671e
pshriva <shrivastavapranjali@gmail.com>
2017-04-13 17:08:56 -0700

    Update ConstructorVisitor.java

--- ConstructorVisitor.java
+++ ConstructorVisitor.java
@@ -6,15 +6,38 @@ import japa.parser.ast.visitor.VoidVisitorAdapter;
 
 
 public class ConstructorVisitor extends VoidVisitorAdapter<Void>{
-	static ArrayList<String> constructors = new ArrayList<String>();
-	static ArrayList<String> constructorDependency = new ArrayList<String>();
+	ArrayList<String> constructors = new ArrayList<String>();
+	ArrayList<String> constructorDependency = new ArrayList<String>();
+	ArrayList<String> classNames = new ArrayList<String>();
+	String javaclassName = null;
+	
+	public void setClassNames(ArrayList<String> classNames) {
+		this.classNames = classNames;
+	}
+	public void setJavaclassName(String javaclassName) {
+		this.javaclassName = javaclassName;
+	}
+	public ArrayList<String> getConstructorDependency() {
+		return constructorDependency;
+	}
+	public ArrayList<String> getConstructors(){
+		//System.out.println(constructors);
+		return constructors;
+	}
 	@Override
 	public void visit(ConstructorDeclaration n, Void arg) {
 		// TODO Auto-generated method stub
 		StringBuffer sb = new StringBuffer();
 		String method = null;
+		String dependent = null;
 		if(n.getParameters() != null && (n.getModifiers()==1 || n.getModifiers() == 1025)){
 			for(int i =0; i<n.getParameters().size(); i++){
+				if(classNames.contains(n.getParameters().get(i).getType().toString())){
+					dependent = javaclassName + " ..> " + n.getParameters().get(i).getType().toString();
+					//System.out.println(dependent);
+					if(constructorDependency.contains(dependent) == false)
+						constructorDependency.add(dependent);
+				}
 				sb.append(n.getParameters().get(i).getId() + " : " + n.getParameters().get(i).getType());
 				if(n.getParameters().size()>1 && i != n.getParameters().size()-1)
 					sb.append(" ");
@@ -28,14 +51,13 @@ public class ConstructorVisitor extends VoidVisitorAdapter<Void>{
 		}
 	}
 	
-	public ArrayList<String> getConstructors(){
-		//System.out.println(constructors);
-		return constructors;
-	}
+	
 
-	public ArrayList<String> getDependency() {
+	/*public ArrayList<String> getDependency(ArrayList<String> classNames2, String javaClassName) {
 		// TODO Auto-generated method stub
-		return null;
-	}
+		this.classNames = classNames2;
+		this.javaclassName = javaClassName;
+		return constructorDependency;
+	}*/
 	
 }

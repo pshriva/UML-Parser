2f3f539f663384e6739b1fa42d2e81c03f9aa6d5
pshriva <shrivastavapranjali@gmail.com>
2017-04-22 12:35:55 -0700

    Add files via upload

---
+++ testcase2/C1.java
@@ -0,0 +1,8 @@
+public class C1 {
+    
+    public void test( A1 a1 ) {
+        // call a1 methods
+    }
+    
+ 
+}
---
+++ testcase2/C2.java
@@ -0,0 +1,8 @@
+public class C2 {
+    
+    public void test( A2 a2) {
+        // call a2 methods
+    }
+    
+}
+ 

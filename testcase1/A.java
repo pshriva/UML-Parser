51daebc0fe5d69bde6017f9e405865df6b2993b4
pshriva <shrivastavapranjali@gmail.com>
2017-04-21 23:15:10 -0700

    Add files via upload

---
+++ testcase1/A.java
@@ -0,0 +1,15 @@
+import java.util.Collection;
+
+public class A {
+ 
+	private int x;
+	 
+	private int[] y;
+	 
+	private Collection<B> b;
+	 
+	private C c;
+	 
+	private Collection<D> d;
+	 
+}
---
+++ testcase1/MyJavaCode/E.java
@@ -0,0 +1,15 @@
+public class E {
+ 
+	//private A a;
+	
+	public void m1(){
+		A d;
+		B b = new B();
+		private Collection<B> b = new B();
+		
+		System.out.println("Hieee");
+	}
+	public void m2(){
+	}
+	 
+}
